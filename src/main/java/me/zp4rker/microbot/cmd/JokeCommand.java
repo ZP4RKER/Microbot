4497906e4f534943f8e09f929a15cf1786c67fcf
ZP4RKER <ZP4RKER@PROGRAMMER.NET>
2017-08-28 21:11:10 +1000

    Trying new joke API

--- src/main/java/me/zp4rker/microbot/cmd/JokeCommand.java
+++ src/main/java/me/zp4rker/microbot/cmd/JokeCommand.java
@@ -1,5 +1,6 @@
 package me.zp4rker.microbot.cmd;
 
+import com.oracle.tools.packager.IOUtils;
 import me.zp4rker.core.command.ICommand;
 import me.zp4rker.core.command.RegisterCommand;
 import me.zp4rker.core.logger.ZLogger;
@@ -9,6 +10,12 @@ import me.zp4rker.microbot.util.MessageUtil;
 import me.zp4rker.microbot.util.YamlUtil;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Message;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
 
 /**
  * @author ZP4RKER
@@ -21,11 +28,24 @@ public class JokeCommand implements ICommand {
     public void onCommand(Message message) {
         MessageUtil.bypassDeleteLogs(message);
         try {
-            Yaml yaml = new Yaml();
+            /*Yaml yaml = new Yaml();
             yaml.loadFromString(YamlUtil.fromUrl("http://tambal.azurewebsites.net/joke/random"));
 
             message.getChannel().sendMessage(new EmbedBuilder()
                     .setTitle(yaml.getString("joke"))
+                    .setColor(ColorUtil.randomColour()).build()).complete();*/
+            URL url = new URL("https://icanhazdadjoke.com/");
+            URLConnection uc = url.openConnection();
+            uc.setRequestProperty("Accept", "text/plain");
+            InputStream is = uc.getInputStream();
+            Scanner scanner = new Scanner(is);
+            StringBuilder sb = new StringBuilder();
+            while (scanner.hasNext()) {
+                sb.append(scanner.next());
+            }
+
+            message.getChannel().sendMessage(new EmbedBuilder()
+                    .setTitle(sb.toString())
                     .setColor(ColorUtil.randomColour()).build()).complete();
         } catch (Exception e) {
             ZLogger.warn("Could not send joke!");
