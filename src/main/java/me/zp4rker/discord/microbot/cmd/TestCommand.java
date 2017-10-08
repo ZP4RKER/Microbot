5a4d6988765074fb55ff1a2e3be7c9ee8b8abf03
ZP4RKER <ZP4RKER@PROGRAMMER.NET>
2017-10-08 22:42:01 +1100

    TC

--- src/main/java/me/zp4rker/discord/microbot/cmd/TestCommand.java
+++ src/main/java/me/zp4rker/discord/microbot/cmd/TestCommand.java
@@ -14,8 +14,8 @@ public class TestCommand implements ICommand {
 
     @RegisterCommand(aliases = "test", showInHelp = false)
     public void onCommand(Message message, String[] args) {
-        String html = "<p>ZP4RKER</p><img src=\"%avatar%\" />";
-        html = html.replace("%avatar%", message.getAuthor().getEffectiveAvatarUrl());
+        String html = "<body style=\"background-color:#CCC\"><p>ZP4RKER</p><img src=\"%avatar%\" /></body>";
+        html = html.replace("%avatar%", "https://zp4rker.me/assets/img/icon.svg");
         message.getTextChannel().sendMessage("HTML: `" + html + "`").queue();
         Html2Image.fromHtml(html).getImageRenderer().setHeight(500).saveImage(new File("test.png"));
         message.getTextChannel().sendFile(new File("test.png"), null).queue();
